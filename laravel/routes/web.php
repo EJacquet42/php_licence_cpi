4a5c7a2095d174fccea6a26d21c2805aa3ec44ee
Léo <leoborie4@gmail.com>
2026-06-09 15:42:21 +0200

    Redirige la page d'accueil / vers /login

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\GeneratorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
